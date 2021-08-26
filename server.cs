7c6c58c8fe85997d1f2ae154c230c7d4589be992
Morgan Metz <68765538+Morgandri1@users.noreply.github.com>
2021-08-26 15:44:32 -0400

    added the server that just has the to-do list

---
+++ server.cs
@@ -0,0 +1,8 @@
+//this will be the server
+//i know nothing about c#
+
+//needs:
+//automatically allow post requests up to 15b
+//allow multiple connections
+//have a prefix system to diferentiate between commands and inputs
+//be stable
